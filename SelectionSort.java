f2e76a1d42c3c112870bd8c8ad47c0c07dc01cb9
Amarjavisettty <23pa1a0592@vishnu.edu.in>
2024-12-18 15:56:57 +0530

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class SelectionSort{
+public static void main(String []args){
+Scanner sc =new Scanner(System.in);
+System.out.println("enter no of elements");
+int n=sc.nextInt();
+int[] A=new int[n];
+System.out.println("enter the elements");
+for(int i=0;i<n;i++)
+A[i]=sc.nextInt();
+SelectionSort(A);
+System.out.println("sortedarray");
+for(int i=0;i<A.length;i++)
+System.out.println(A[i]+"");
+}
+static void SelectionSort(int[] A){
+int n=A.length;
+for(int i=0;i<n-1;i++){
+int z=i;
+for(int j=i+1;j<n;j++){
+if(A[j]<A[z]){
+z=j;
+}
+}
+int temp=A[z];
+A[z]=A[i];
+A[i]=temp;
+}
+}
+}
+
